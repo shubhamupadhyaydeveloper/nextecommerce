9a01a4d9c12963fdb010df0a9f3407718f57a87f
ShuhbamUpadhyayDeveloper <shubhamwork48@gmail.com>
2024-05-20 12:18:09 +0530

    changes

--- src/models/downloadVerification.model.ts
+++ src/models/downloadVerification.model.ts
@@ -1,12 +1,1 @@
-import mongoose,{Model} from "mongoose";
-import { TdownloadVerification } from "@/types/types";
-
-const downloadVerificationSchema = new  mongoose.Schema<TdownloadVerification>({
-     productId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref :  'Product'
-     }
-},{timestamps :  true})
-
-const downloadVerifcation:Model<TdownloadVerification> = mongoose.model('downloadVerifcation',downloadVerificationSchema)
-export default downloadVerifcation
+ 
